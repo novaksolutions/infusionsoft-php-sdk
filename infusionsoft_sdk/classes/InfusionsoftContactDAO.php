054bd747024260173a792c5fc71f6bf52f704359
joey.novak <joey.novak@91841d3c-ca5e-11de-a9c8-2fbcfba63733>
2010-01-13 06:41:42 +0000

    Delete Everything!
    git-svn-id: http://php-infusionsoft-sdk.googlecode.com/svn/trunk@13 91841d3c-ca5e-11de-a9c8-2fbcfba63733

--- infusionsoft_sdk/classes/InfusionsoftContactDAO.php
+++
@@ -1,6 +0,0 @@
-<?php
-class InfusionsoftContactDAO extends InfusionsoftDataDAO{
-	public function __construct(){
-		parent::__construct('Contact');		
-	}	
-}
--- infusionsoft_sdk/config.sample.php
+++
@@ -1,9 +0,0 @@
-<?php
-$infusionsoft_host = 'joey.infusionsoft.com';
-$infusionsoft_api_key = '(YOUR API KEY)';
-
-//Add your custom fields here if you want to retrieve them via the API.
-//$GLOBALS['infusionsoft_custom_fields']['Job'][] = '_ShippingStatus';
-//$GLOBALS['infusionsoft_custom_fields']['Job'][] = '_UPSTrackingNumber';
-?>
-
