4839b8c9b5d8dcf506e6f53a8c372727110e2e67
joey.novak <joey.novak@91841d3c-ca5e-11de-a9c8-2fbcfba63733>
2010-04-06 18:27:36 +0000

    Updated the config.sample.php to use AppPool and to include instructions for custom fields.
    git-svn-id: http://php-infusionsoft-sdk.googlecode.com/svn/trunk@21 91841d3c-ca5e-11de-a9c8-2fbcfba63733

--- Infusionsoft/config.sample.php
+++ Infusionsoft/config.sample.php
@@ -1,9 +1,10 @@
 <?php
+
 $infusionsoft_host = 'joey.infusionsoft.com';
-$infusionsoft_api_key = '(YOUR API KEY)';
+$infusionsoft_api_key = 'YOUR API KEY GOES HERE';
 
-//Add your custom fields here if you want to retrieve them via the API.
-//$GLOBALS['infusionsoft_custom_fields']['Job'][] = '_ShippingStatus';
-//$GLOBALS['infusionsoft_custom_fields']['Job'][] = '_UPSTrackingNumber';
-?>
+//To Add Custom Fields, use the addCustomField method like below.
+//Infusionsoft_Contact::addCustomField('_LeadScore');
 
+//Below is just some magic...  Unless you are going to be communicating with more than one APP at the SAME TIME.  You can ignore it.
+Infusionsoft_AppPool::addApp(new Infusionsoft_App($infusionsoft_host, $infusionsoft_api_key, 443));
