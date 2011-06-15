033b71815b5632e31b9b1597d13cf0b767e8265c
joey.novak@gmail.com <joey.novak@gmail.com@91841d3c-ca5e-11de-a9c8-2fbcfba63733>
2011-06-15 17:38:33 +0000

    Whoops, missed custom field service...
    git-svn-id: http://php-infusionsoft-sdk.googlecode.com/svn/trunk@43 91841d3c-ca5e-11de-a9c8-2fbcfba63733

---
+++ Infusionsoft/CustomFieldService.php
@@ -0,0 +1,52 @@
+<?php
+class Infusionsoft_CustomFieldService extends Infusionsoft_DataService{
+
+    static $DataType_Name = 10;
+    static $DataType_Email = 19;
+    static $DataType_PhoneNumber = 1;
+    static $DataType_Website = 18;
+    static $DataType_SocialSecurityNumber = 2;
+    static $DataType_State = 5;
+
+    static $DataType_WholeNumber = 12;
+    static $DataType_DecimalNumber = 11;
+    static $DataType_Currency = 3;
+    static $DataType_Percent = 4;
+
+    static $DataType_Radio = 20;
+    static $DataType_Dropdown = 21;
+    static $DataType_Text = 15;
+    static $DataType_TextArea = 16;
+    static $DataType_YesNo = 6;
+    static $DataType_Drilldown = 23;
+    static $DataType_List = 17;
+
+    static $DataType_Date = 13;
+    static $DataType_DateTime = 14;
+    static $DataType_DayOfWeek = 9;
+    static $DataType_Year = 7;
+    static $DataType_Month = 8;
+
+
+    static $DataType_User = 22;
+    static $DataType_UserListBox = 25;
+
+	public static function getCustomFields(Infusionsoft_Generated_Base $object, $dataType = null, Infusionsoft_App $app = null){
+        if(!property_exists($object, 'customFieldFormId')){
+            throw new Infusionsoft_Exception(get_class($object) . ' does not have Custom Fields.');
+        }
+
+        $dataFormField = new Infusionsoft_DataFormField();
+        if($object->getAppPoolAppKey() != null){
+            $dataFormField->setAppPoolAppKey($object->getAppPoolAppKey());
+        }
+
+        $conditions = array('FormId' => $object->customFieldFormId);
+        if($dataType != null){
+            $conditions['DataType'] = $dataType;
+        }
+		$out = parent::query(new Infusionsoft_DataFormField(), $conditions);
+
+		return $out;	
+	}
+}
---
+++ Infusionsoft/examples/explore_custom_fields.php
@@ -0,0 +1,16 @@
+<?php
+    include('../infusionsoft.php');
+	include('object_editor_all_tables.php');	
+	include('../tests/testUtils.php');
+
+    $customFields = Infusionsoft_CustomFieldService::getCustomFields(new Infusionsoft_Contact(), Infusionsoft_CustomFieldService::$DataType_Dropdown);
+    var_dump($customFields);
+
+    foreach($customFields as $customField){
+        if($customField->Name == 'Test'){
+            $customFieldValues = $customField->getCustomFieldValues();
+            $customFieldValues[] = rand(1, 100) . ' Value';
+            $customField->setCustomFieldValues($customFieldValues);
+            //$customField->save();
+        }
+    }
