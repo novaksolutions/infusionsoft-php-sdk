770b2ab81ade16275c8fb2d1613373d23397fd69
joey.novak@gmail.com <joey.novak@gmail.com@91841d3c-ca5e-11de-a9c8-2fbcfba63733>
2012-03-06 01:58:47 +0000

    I hate svn...
    git-svn-id: http://php-infusionsoft-sdk.googlecode.com/svn/trunk@79 91841d3c-ca5e-11de-a9c8-2fbcfba63733

---
+++ Infusionsoft/Generated/SubscriptionPlan.php
@@ -0,0 +1,29 @@
+<?php
+class Infusionsoft_Generated_SubscriptionPlan extends Infusionsoft_Generated_Base{
+    static $Cycle_WEEK  = 3;
+    static $Cycle_MONTH = 2;
+    static $Cycle_YEAR  = 1;
+    static $Cycle_DAY   = 6;
+
+    protected static $tableFields = array('Id', 'ProductId', 'Cycle', 'Frequency', 'PreAuthorizeAmount', 'Prorate');
+
+    public function __construct($id = null, $app = null){    	    	
+    	parent::__construct('SubscriptionPlan', $id, $app);
+    }
+    
+    public function getFields(){
+		return self::$tableFields;	
+	}
+	
+	public function addCustomField($name){
+		self::$tableFields[] = $name;
+	}
+
+    public function removeField($fieldName){
+        $fieldIndex = array_search($fieldName, self::$tableFields);
+        if($fieldIndex !== false){
+            unset(self::$tableFields[$fieldIndex]);
+            self::$tableFields = array_values(self::$tableFields);
+        }
+    }
+}
---
+++ Infusionsoft/SubscriptionPlan.php
@@ -0,0 +1,7 @@
+<?php
+class Infusionsoft_SubscriptionPlan extends Infusionsoft_Generated_SubscriptionPlan{
+    public function __construct($id = null, $app = null){
+    	parent::__construct($id, $app);    	    	
+    }
+}
+
