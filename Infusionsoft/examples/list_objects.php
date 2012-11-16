e417b8a1ef3da4c6c6176b0e18fe84c03e810b81
joey <joey.novak@gmail.com>
2012-11-16 13:23:18 -0700

    Added Create Order and List Objects

---
+++ Infusionsoft/examples/list_objects.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * @var Infusionsoft_Generated_Base $object
+ */
+include('../infusionsoft.php');
+	include('object_editor_all_tables.php');
+	include('../tests/testUtils.php');
+	?>
+<form>
+    Table:<br/>
+    <select name="object">
+        <?php
+            global $all_tables;
+            sort($all_tables);
+            foreach($all_tables as $table){
+                ?><option value="<?php echo $table; ?>"><?php echo $table; ?></option><?php
+            }
+        ?>
+    </select><br/>
+    <input type="submit"/>
+</form>
+<?php
+
+if(isset($_GET['object'])){
+    $class_name = "Infusionsoft_" . $_GET['object'];
+    $object = new $class_name();
+
+    $objects = Infusionsoft_DataService::query(new $class_name(), array('Id' => '%'));
+    ?>
+        <table>
+            <thead>
+                <tr>
+                    <?php
+                        foreach($object->getFields() as $field){
+                            ?><th><?=$field?></th><?php
+                        }
+                    ?>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                foreach($objects as $object){
+                    ?><tr><?php
+                    foreach($object->toArray() as $data){
+                        ?><td><?=htmlspecialchars($data)?></td><?php
+                    }
+                    ?></tr><?php
+                }
+                ?>
+          </tbody>
+        </table>
+    <?php
+}
