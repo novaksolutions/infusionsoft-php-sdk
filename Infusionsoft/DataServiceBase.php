7f9ef8f8ba4ff8eff9a42f1b8d4be49a44c74863
Jordan Novak <freezey@gmail.com>
2013-01-31 16:07:50 -0600

    Put AuthenticateUser method back into DataService Base

--- Infusionsoft/DataServiceBase.php
+++ Infusionsoft/DataServiceBase.php
@@ -1,4 +1,12 @@
 <?php
 class Infusionsoft_DataServiceBase extends Infusionsoft_Service{
 
+    public static function authenticateUser($username, $passwordHash, Infusionsoft_App $app = null){
+        $params = array(
+            $username,
+            $passwordHash
+        );
+        return parent::send($app, "DataService.authenticateUser", $params);
+    }
+
 }
