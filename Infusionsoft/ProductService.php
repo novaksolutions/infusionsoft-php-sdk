581f8d99fae41088e246b0cfbbbcb4f1bbb55ed9
Jordan Novak <freezey@gmail.com>
2013-02-04 11:40:47 -0600

    Include getInventory method in ProductService

--- Infusionsoft/ProductService.php
+++ Infusionsoft/ProductService.php
@@ -3,4 +3,14 @@ class Infusionsoft_ProductService extends Infusionsoft_ProductServiceBase{
 	public static function ping(Infusionsoft_App $app = null){
 		return parent::ping('ProductService', $app);
 	}
+
+    public static function getInventory($id, Infusionsoft_App $app = null){
+        $app = parent::getObjectOrDefaultAppIfNull($app);
+
+        $params = array(
+            (int) $id
+        );
+
+        return $app->send('ProductService.getInventory' , $params, true);
+    }
 }
