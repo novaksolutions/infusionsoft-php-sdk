007fc0fdc09b675c8bc1986e4d8422c5d3deeb72
joey <joey.novak@gmail.com>
2013-05-08 09:51:13 -0700

    Created APIAffiliateService Test Mock

--- TestMocks/Infusionsoft/APIAffiliateService.php
+++ TestMocks/Infusionsoft/APIAffiliateService.php
@@ -1,46 +1,47 @@
 <?php
-class Infusionsoft_APIAffiliateServiceBase extends Infusionsoft_Service{
+
+
+class Infusionsoft_APIAffiliateService extends Infusionsoft_APIAffiliateServiceBase{
+    static $data;
+
+    public static function setData($data){
+        Infusionsoft_APIAffiliateService::$data = $data;
+    }
 
     public static function affPayouts($affiliateId, $filterStartDate, $filterEndDate, Infusionsoft_App $app = null){
         $params = array(
-            (int) $affiliateId, 
-            $filterStartDate, 
+            (int) $affiliateId,
+            $filterStartDate,
             $filterEndDate
         );
 
         return parent::send($app, "APIAffiliateService.affPayouts", $params);
     }
-    
-    public static function affCommissions($affiliateId, $filterStartDate, $filterEndDate, Infusionsoft_App $app = null){
-        $params = array(
-            (int) $affiliateId, 
-            $filterStartDate, 
-            $filterEndDate
-        );
 
-        return parent::send($app, "APIAffiliateService.affCommissions", $params, null, true);
+    public static function affCommissions($affiliateId, $filterStartDate, $filterEndDate, Infusionsoft_App $app = null){
+        return Infusionsoft_APIAffiliateService::$data;
     }
-    
+
     public static function affClawbacks($affiliateId, $filterStartDate, $filterEndDate, Infusionsoft_App $app = null){
         $params = array(
-            (int) $affiliateId, 
-            $filterStartDate, 
+            (int) $affiliateId,
+            $filterStartDate,
             $filterEndDate
         );
 
         return parent::send($app, "APIAffiliateService.affClawbacks", $params);
     }
-    
+
     public static function affSummary($affiliateIds, $filterStartDate, $filterEndDate, Infusionsoft_App $app = null){
         $params = array(
-            $affiliateIds, 
-            $filterStartDate, 
+            $affiliateIds,
+            $filterStartDate,
             $filterEndDate
         );
 
         return parent::send($app, "APIAffiliateService.affSummary", $params);
     }
-    
+
     public static function affRunningTotals($affiliateIds, Infusionsoft_App $app = null){
         $params = array(
             $affiliateIds
@@ -48,17 +49,18 @@ class Infusionsoft_APIAffiliateServiceBase extends Infusionsoft_Service{
 
         return parent::send($app, "APIAffiliateService.affRunningTotals", $params);
     }
-    
+
     public static function updatePhoneStats($firstName, $lastName, $calls, $totalTime, $averageTime, Infusionsoft_App $app = null){
         $params = array(
-            $firstName, 
-            $lastName, 
-            (int) $calls, 
-            $totalTime, 
+            $firstName,
+            $lastName,
+            (int) $calls,
+            $totalTime,
             $averageTime
         );
 
         return parent::send($app, "APIAffiliateService.updatePhoneStats", $params);
     }
-    
 }
+
+
