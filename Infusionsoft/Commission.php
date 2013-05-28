8df7be037b60c94173d20322e76fb4ee5f375754
Jordan Novak <freezey@gmail.com>
2013-05-28 16:09:12 -0500

    added loadFromArray method (over-rides parent method) that sets the Id field to a concatenated string of existing data (I still have to test that we are never going to get a non-unique ID using this process)

--- Infusionsoft/Commission.php
+++ Infusionsoft/Commission.php
@@ -5,11 +5,25 @@
  * Date: 5/3/13
  * Time: 12:42 PM
  * To change this template use File | Settings | File Templates.
+ * @property String $Id
+ * @property String $AffiliateId
+ * @property String $ContactLastName
+ * @property String $SoldByLastName
+ * @property String $Description
+ * @property String $ContactFirstName
+ * @property String $AmtEarned
+ * @property String $InvoiceId
+ * @property String $ProductName
+ * @property String $ContactId
+ * @property String $SoldByFirstName
+ * @property String $DateEarned
+ * @property String $SaleAffId
  */
 
 class Infusionsoft_Commission extends Infusionsoft_Generated_Base {
 
     protected static $tableFields = array(
+        "Id",
         "AffiliateId",
         "ContactLastName",
         "SoldByLastName",
@@ -27,6 +41,9 @@ class Infusionsoft_Commission extends Infusionsoft_Generated_Base {
     //Commissions don't actually have ids in Infusionsoft, so $idString is i of the form $affId/$date/$index
     public function __construct($idString = null, $app = null){
         $this->table = 'Commission';
+        if (is_array($idString)){
+            $this->loadFromArray($idString);
+        }
         if ($idString != null) {
             $this->load($idString, $app);
         }
@@ -87,4 +104,16 @@ class Infusionsoft_Commission extends Infusionsoft_Generated_Base {
             throw new Infusionsoft_Exception("Invalid commission Id");
     }
 
+    public function loadFromArray($data){
+        if (!array_key_exists('AffiliateId', $data) || !array_key_exists('InvoiceId', $data) || !array_key_exists('DateEarned', $data)){
+            throw new Exception('Lacking necessary fields to create UniqueID for Commissions Object.');
+        }
+        $data['Id'] = 'AffId:' . $data['AffiliateId'] . 'InvoiceId:' . $data['InvoiceId'] . 'DateEarned:' . $data['DateEarned'];
+        foreach ($this->getFields() as $field){
+            $this->$field = NULL;
+            if ($data && is_array($data) && isset($data[$field])){
+                $this->$field = $data[$field];
+            }
+        }
+    }
 }
