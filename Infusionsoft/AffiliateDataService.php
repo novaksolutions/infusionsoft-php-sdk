d43dc824d78082cf90293f8000d321d5bd7dbd7b
Jordan Novak <freezey@gmail.com>
2013-05-31 16:17:32 -0500

    AffiliateDataService improved startDate and endDate algorithms cache affiliates to prevent multiple queries

--- Infusionsoft/AffiliateDataService.php
+++ Infusionsoft/AffiliateDataService.php
@@ -13,18 +13,16 @@ class Infusionsoft_AffiliateDataService extends Infusionsoft_Service {
     static $done = false;
     static $first_order_date = null;
     static $earliest_month_to_query = null;
+    static $affiliates_cache = array();
     //'DateEarned' is the only field for which ordering is supported.
     //If month is 0, all commissions for the current month will be returned
     public static function queryWithOrderBy($object, $queryData, $orderByField, $ascending = true, $limit = 1000, $month = 0, $returnFields = false, Infusionsoft_App $app = null){
         //Calculate beginning of this month
-        $startDate = new DateTime(date('Y-m-01')); //first day of this month
-        $startDate = date_sub($startDate, new DateInterval('P'.$month.'M')); //$month months before the start of this month
-        $startDate = $startDate->format(Infusionsoft_Service::apiDateFormat); //format as string
+        $startDate = date('Y-m-01', strtotime(" - $month months"));
+        $startDate = date(Infusionsoft_Service::apiDateFormat, strtotime($startDate));
 
-        $endDate = new DateTime(date('Y-m-01')); //first of this month
-        $endDate = date_sub($endDate, new DateInterval('P'. $month . 'M')); //same as$startDate
-        $endDate = date_add($endDate, new DateInterval('P1M')); // one month later
-        $endDate = $endDate->format(Infusionsoft_Service::apiDateFormat);
+        $endDate = date('Y-m-d', strtotime($startDate . ' + 1 month - 1 day'));
+        $endDate = date(Infusionsoft_Service::apiDateFormat, strtotime($endDate));
 
         if (self::$first_run === true && !empty(self::$first_order_date) && strtotime($startDate) < strtotime(self::$first_order_date)){
             self::$done = true;
@@ -35,20 +33,23 @@ class Infusionsoft_AffiliateDataService extends Infusionsoft_Service {
             }
         }
         //get an array of all affiliates
-        $affiliates = array();
-        $page = 0;
-        do {
-            $affiliatesPage = Infusionsoft_DataService::query(
-                new Infusionsoft_Affiliate(),
-                array('Id' => '%'),
-                1000,
-                $page,
-                array('Id'),
-                $app
-            );
-            $page += 1;
-            $affiliates = array_merge($affiliates, $affiliatesPage);
-        } while (sizeof($affiliatesPage) >= 1000);
+        $affiliates = self::$affiliates_cache;
+        if (empty($affiliates)){
+            $page = 0;
+            do {
+                $affiliatesPage = Infusionsoft_DataService::query(
+                    new Infusionsoft_Affiliate(),
+                    array('Id' => '%'),
+                    1000,
+                    $page,
+                    array('Id'),
+                    $app
+                );
+                $page += 1;
+                $affiliates = array_merge($affiliates, $affiliatesPage);
+            } while (sizeof($affiliatesPage) >= 1000);
+        }
+
         if (self::$first_run === true && self::$first_order_date === null){
             $firstOrder = Infusionsoft_DataService::queryWithOrderBy(new Infusionsoft_Job(), array('Id' => '%'), 'Id', true, 1);
             if (!empty($firstOrder)){
