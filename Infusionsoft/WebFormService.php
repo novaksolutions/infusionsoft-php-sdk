075fc30ee91fa0fd9448bfdd0ceb1001ccfce34c
Jacob Allred <jacob.allred@corbanworks.com>
2014-01-28 13:33:26 -0600

    Add $serviceName to WebFormService ping function definition

--- Infusionsoft/WebFormService.php
+++ Infusionsoft/WebFormService.php
@@ -2,15 +2,15 @@
 
 class Infusionsoft_WebFormService extends Infusionsoft_WebFormServiceBase
 {
-    
-    public static function ping(Infusionsoft_App $app = null)
+
+    public static function ping($serviceName = 'WebFormService', Infusionsoft_App $app = null)
     {
-        parent::ping('WebFormService', $app);
+        parent::ping($serviceName, $app);
     }
 
     /**
      * Get a list of all web form names and IDs.
-     * 
+     *
      * @param Infusionsoft_App $app
      * @return array List of web form names and IDs.
      */
@@ -22,10 +22,10 @@ class Infusionsoft_WebFormService extends Infusionsoft_WebFormServiceBase
 
     /**
      * Get the web form's full HTML.
-     * 
+     *
      * Get the full HTML for the specified web form ID. This includes all HTML
      * tags and is meant to be used as a standalone webpage.
-     * 
+     *
      * @param int $webFormId The ID from Infusionsoft_WebFormService::getMap.
      * @param Infusionsoft_App $app
      * @return string Full HTML for the web form.
@@ -38,13 +38,13 @@ class Infusionsoft_WebFormService extends Infusionsoft_WebFormServiceBase
 
     /**
      * Get the web form's JavaScript snippet.
-     * 
-     * Instead of having to use the HTML code, you can get the JavaScript 
-     * snippet. The web form displayed by the snippet will be updated 
+     *
+     * Instead of having to use the HTML code, you can get the JavaScript
+     * snippet. The web form displayed by the snippet will be updated
      * automatically when it is updated in Infusionsoft.
-     * 
+     *
      * @author Jacob Allred <jacob@novaksolutions.com>
-     * 
+     *
      * @param int $webFormId The ID from Infusionsoft_WebFormService::getMap.
      * @param Infusionsoft_App $app
      * @return string JavaScript snippet for the web form.
@@ -52,31 +52,31 @@ class Infusionsoft_WebFormService extends Infusionsoft_WebFormServiceBase
     public static function getJavaScript($webFormId, Infusionsoft_App $app = null)
     {
         $app = parent::getObjectOrDefaultAppIfNull($app);
-        
+
         /*
          * The API doesn't provide a method of getting the JavaScript snippet.
          * Instead, we are going to get the HTML, find the form GUID, and create
          * the JavaScript snippet on our own.
          */
-        
+
         // Get the HTML
         $html = Infusionsoft_WebFormService::getHTML($webFormId, $app);
-        
+
         // Create our search string
         $search = $app->getHostname() . '/app/form/process/';
-        
+
         // Find the start and stop position of the form GUID
         $start = strpos($html, $search) + strlen($search);
         $stop = strpos(substr($html, $start), '"');
-        
+
         // Pull out the GUID
         $guid = substr($html, $start, $stop);
-        
+
         // Put together the JavaScript snippet
         $snippet = '<script type="text/javascript" src="https://';
         $snippet .= $app->getHostname();
         $snippet .= '/app/form/iframe/' . $guid . '"></script>';
-        
+
         return $snippet;
     }
 }
