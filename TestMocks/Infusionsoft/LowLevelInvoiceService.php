11887a2c883f72a261a8d36c02bde737ff2c064a
Jordan Novak <freezey@gmail.com>
2014-01-29 14:39:39 -0600

    add updateJobRecurringNextBillDate to TestMocks InvoiceService

--- TestMocks/Infusionsoft/LowLevelInvoiceService.php
+++ TestMocks/Infusionsoft/LowLevelInvoiceService.php
@@ -53,4 +53,13 @@ class Infusionsoft_LowLevelInvoiceService extends Infusionsoft_LowLevelMockServi
 
         return $invoice->Id;
     }
+
+    public function updateJobRecurringNextBillDate($args){
+        array_shift($args);
+        list($subscriptionId, $nextBillDate) = $args;
+        $subscription = new Infusionsoft_RecurringOrder($subscriptionId);
+        $subscription->NextBillDate = $nextBillDate;
+        $subscription->save();
+        return true;
+    }
 }
