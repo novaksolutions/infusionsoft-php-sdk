077beada9ca990d24caba6764a1f0d83fd1724d5
Zack Katz <zack@katzwebservices.com>
2014-01-28 21:43:16 -0700

    Check whether functions and class before loading file

--- Infusionsoft/infusionsoft.php
+++ Infusionsoft/infusionsoft.php
@@ -1,7 +1,10 @@
 <?php
 
 require_once('Classloader.php');
-require_once('xmlrpc.inc');
+
+if(!function_exists('xmlrpc_encode_entitites') && !class_exists('xmlrpcresp')) {
+	require_once('xmlrpc.inc');
+}
 
 $classLoader = new Infusionsoft_Classloader();
 spl_autoload_register(array(&$classLoader, "loadClass"));
