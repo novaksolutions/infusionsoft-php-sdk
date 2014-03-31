ea661e0b8a803b6b652be4d0c38c0f80552270ba
joey <joey.novak@gmail.com>
2014-03-31 09:19:09 -0700

    Changed Affiliate Service to behave normally and only return empty pages when there is no data.  Also added SavedSearchDataService

--- Infusionsoft/AffiliateDataService.php
+++ Infusionsoft/AffiliateDataService.php
@@ -9,20 +9,29 @@
 
 
 class Infusionsoft_AffiliateDataService extends Infusionsoft_Service {
-    static $first_run = false;
-    static $done = false;
-    static $first_order_date = null;
-    static $earliest_month_to_query = null;
-    static $affiliates_cache = array();
-    static $orderByField = 'DateEarned';
+    static $firstOrderDate = null;
+    static $affiliatesCache = null;
+    static $orderByField = null;
+    static $pageToMonthOMaps = array();
     //'DateEarned' is the only field for which ordering is supported.
     //If month is 0, all commissions for the current month will be returned
-    public static function queryWithOrderBy($object, $queryData, $orderByField, $ascending = true, $limit = 1000, $month = 0, $returnFields = false, Infusionsoft_App $app = null){
-        //Set a hard date for earliest_month_to_query so that we don't get into infinite loops trying to find commissions in the 1700's (this happens when you sync an empty application)
-        if (empty(self::$earliest_month_to_query)){
-            self::$earliest_month_to_query = '2010-01-01';
-        }
+    public static function queryWithOrderBy($object, $queryData, $orderByField = null, $ascending = true, $limit = 1000, $page = 0, $returnFields = false, Infusionsoft_App $app = null){
+        if(self::$firstOrderDate === null) self::setFirstOrderDate();
+        if(self::$firstOrderDate === false) return array();
+        if(self::$affiliatesCache === null) self::populateAffiliateCache();
+        if(self::$affiliatesCache === false) return array();
 
+        $month = self::getMonthForPage($object, $page);
+        $records = self::queryByMonthWithOrderBy($object, $queryData, $orderByField, $ascending, $limit, $month, $returnFields, $app);
+        while($records !== false && count($records) == 0){
+            $month++;
+            $records = self::queryByMonthWithOrderBy($object, $queryData, $orderByField, $ascending, $limit, $month, $returnFields, $app);
+        }
+        self::setPageForMonth($object, $month, $page);
+        return $records;
+    }
+
+    public static function queryByMonthWithOrderBy($object, $queryData, $orderByField, $ascending = true, $limit = 1000, $month = 0, $returnFields = false, Infusionsoft_App $app = null){
         //Calculate beginning of this month
         $startDate = date('Y-m-01 00:00:00', strtotime(" - $month months"));
         $startDate = date(Infusionsoft_Service::apiDateFormat, strtotime($startDate));
@@ -30,44 +39,13 @@ class Infusionsoft_AffiliateDataService extends Infusionsoft_Service {
         $endDate = date('Y-m-d 23:59:59', strtotime($startDate . ' + 1 month - 1 day'));
         $endDate = date(Infusionsoft_Service::apiDateFormat, strtotime($endDate));
 
-        if (self::$first_run === true && !empty(self::$first_order_date) && strtotime($startDate) < strtotime(self::$first_order_date)){
-            self::$done = true;
-        }
-        if (!empty(self::$earliest_month_to_query)){
-            if (strtotime($startDate) < strtotime(self::$earliest_month_to_query)){
-                self::$done = true;
-            }
-        }
-        //get an array of all affiliates
-        $affiliates = self::$affiliates_cache;
-        if (empty($affiliates)){
-            $page = 0;
-            do {
-                $affiliatesPage = Infusionsoft_DataService::query(
-                    new Infusionsoft_Affiliate(),
-                    array('Id' => '%'),
-                    1000,
-                    $page,
-                    array('Id'),
-                    $app
-                );
-                $page += 1;
-                $affiliates = array_merge($affiliates, $affiliatesPage);
-            } while (sizeof($affiliatesPage) >= 1000);
+        if (strtotime($startDate) < strtotime(self::$firstOrderDate) || strtotime($startDate) < strtotime("2000-01-01")){
+            return false;
         }
 
-        if (self::$first_run === true && self::$first_order_date === null){
-            $firstOrder = Infusionsoft_DataService::queryWithOrderBy(new Infusionsoft_Job(), array('Id' => '%'), 'Id', true, 1);
-            if (!empty($firstOrder)){
-                self::$first_order_date = $firstOrder[0]->DateCreated;
-            } else {
-                self::$first_order_date = false;
-            }
-        }
-
-        //Now get all of the commissions from each one
+        //Now get all of the commissions or clawbacks from each one
         $objects = array();
-        foreach ($affiliates as $affiliate) {
+        foreach (self::$affiliatesCache as $affiliate) {
             if (get_class($object) == 'Infusionsoft_Commission'){
                 $objects = array_merge(
                     $objects,
@@ -81,12 +59,70 @@ class Infusionsoft_AffiliateDataService extends Infusionsoft_Service {
             }
         }
 
-        self::$orderByField = $orderByField;
-        usort($objects, array('Infusionsoft_AffiliateDataService', 'sortCommissions'));
+        if($orderByField != null){
+            self::$orderByField = $orderByField;
+            usort($objects, array('Infusionsoft_AffiliateDataService', 'sortCommissions'));
+        }
+
         return $objects;
     }
 
     public static function sortCommissions($a, $b){
         return $a->{self::$orderByField} < $b->{self::$orderByField};
+    }
+
+    private static function getMonthForPage($object, $page){
+        $className = get_class($object);
+        if(!isset(self::$pageToMonthOMaps[$className])){
+            self::$pageToMonthOMaps[$className] = array();
+            if($page == 0){
+                self::$pageToMonthOMaps[$className][$page] = 0;
+            } else {
+                throw new Exception("The Affiliate Data Service MUST have pages accessed in order.");
+            }
+        }
+
+
+        if(!isset(self::$pageToMonthOMaps[$className][$page])){
+            end(self::$pageToMonthOMaps[$className]);
+            return self::$pageToMonthOMaps[$className][key(self::$pageToMonthOMaps[$className])] + 1;
+        } else{
+            return self::$pageToMonthOMaps[$className][$page];
+        }
+    }
+
+    private static function setPageForMonth($object, $month, $page){
+        $className = get_class($object);
+        if(!isset(self::$pageToMonthOMaps[$className])){
+            self::$pageToMonthOMaps[$className] = array();
+        }
+        self::$pageToMonthOMaps[$className][$page][$month];
+    }
+
+    private static function setFirstOrderDate(){
+        $firstOrder = Infusionsoft_DataService::queryWithOrderBy(new Infusionsoft_Job(), array('Id' => '%'), 'DateCreated', true, 1);
+        if (!empty($firstOrder)){
+            $firstOrder = array_shift($firstOrder);
+            self::$firstOrderDate = $firstOrder->DateCreated;
+        } else {
+            self::$firstOrderDate = false;
+        }
+    }
+
+    private static function populateAffiliateCache(){
+        $page = 0;
+        $pageSize = 1000;
+        self::$affiliatesCache = array();
+        do {
+            $affiliatesPage = Infusionsoft_DataService::query(
+                new Infusionsoft_Affiliate(),
+                array('Id' => '%'),
+                $pageSize,
+                $page,
+                array('Id')
+            );
+            $page += 1;
+            self::$affiliatesCache = array_merge(self::$affiliatesCache, $affiliatesPage);
+        } while (sizeof($affiliatesPage) == $pageSize);
     }
 }
