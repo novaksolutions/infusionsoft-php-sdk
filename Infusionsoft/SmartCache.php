30e78491c0104482d3aae0065d536999d63495c9
joey <joey.novak@gmail.com>
2014-09-11 03:09:27 -0700

    Infusionsoft Sdk moved from InfusionsoftSdk plugin to Infusionsoft Plugin

--- Infusionsoft/SmartCache.php
+++ Infusionsoft/SmartCache.php
@@ -7,7 +7,7 @@ class Infusionsoft_SmartCache{
 
     public function __construct($name, $ttl = 300, $dir = 'cache'){
         if (strpos($dir, '/') !== 0 && strpos($dir, ':') !== 1){
-            $dir = APP . 'Vendor' . DS . 'php-infusionsoft-sdk' . DS . 'InfusionsoftSdk.Infusionsoft' . DS . $dir;
+            $dir = APP . 'Vendor' . DS . 'php-infusionsoft-sdk' . DS . 'Infusionsoft.Infusionsoft' . DS . $dir;
         }
         $this->ttl = $ttl;
         $this->dir = $dir;
