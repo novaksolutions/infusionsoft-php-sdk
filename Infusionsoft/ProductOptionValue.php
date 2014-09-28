138290cfc7bfdd9ab0fe67ea37f347a50de19d2a
Andrew Steele <cronoh@gmail.com>
2014-09-28 15:17:19 -0700

    Added base classes for ProductOption and ProductOptionValue

---
+++ Infusionsoft/ProductOptionValue.php
@@ -0,0 +1,6 @@
+<?php
+class Infusionsoft_ProductOptionValue extends Infusionsoft_Generated_ProductOptionValue {
+    public function __construct($id = null, $app = null){
+    	parent::__construct($id, $app);    	    	
+    }
+}
