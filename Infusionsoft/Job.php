101274d1d4e6536c18713b591e43c43d34c3bacb
Joey Novak <joey.novak@gmail.com>
2014-11-17 23:28:01 -0700

    Job->save automatically calls InvoiceService::createBlankOrder if it's a new order.

--- Infusionsoft/Job.php
+++ Infusionsoft/Job.php
@@ -11,5 +11,18 @@ class Infusionsoft_Job extends Infusionsoft_Generated_Job{
             self::addCustomField($name);
         }
 	}
+
+    public function save($app = null){
+        if($this->Id == ''){
+            $invoiceId = Infusionsoft_InvoiceService::createBlankOrder($this->ContactId, $this->JobNotes, $this->DateCreated);
+            $invoice = new Infusionsoft_Invoice($invoiceId);
+            $this->Id = $invoice->JobId;
+            $result = true;
+        } else {
+            $result = parent::save($app);
+        }
+
+        return $result;
+    }
 }
 
